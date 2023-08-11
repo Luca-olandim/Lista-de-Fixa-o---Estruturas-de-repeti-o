88aaf3c4aac34e2c543eb65d2246dcc7bbaa2350
Luca-olandim <127211138+Luca-olandim@users.noreply.github.com>
2023-08-11 12:43:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function anoNovo(){
+
+}
+function tabuada(){
+
+}
+
+function somaPares(){
+
+}
+
+function primos(){
+    let num = Number(document.getElementById("entrada").value)
+    if (num % 2 == 0){
+        document.getElementById("resposta").innerHTML = num+" Não é primo."
+    }
+    else{
+        document.getElementById("resposta").innerHTML = num+" É primo."
+    }
+}
+
+function fibonacci(){
+
+}
